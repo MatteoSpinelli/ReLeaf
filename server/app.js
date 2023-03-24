8c2ee7eb6dc38930eefceb6cd33a74f649c9cb5c
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-24 19:51:00 +0100

    fix: vercel config headers

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const apiV1Router = require("./routes/api/v1");
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({ origin: "*" }));
 
 // api route
 app.use("/api/v1", apiV1Router);
