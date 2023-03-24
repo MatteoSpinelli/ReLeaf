6a3b524234849ff016931f3e5c8362d4ab8bd5bd
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-24 19:58:22 +0100

    fix: vercel config headers

--- server/routes/api/v1/index.js
+++ server/routes/api/v1/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET /api/v1 - Index of the api */
 router.get("/", function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json({ status: 200, message: "hello from server" });
 });
 
