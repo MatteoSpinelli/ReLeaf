e9fb901e638580b85e8ff67b17e0dec0cfcef8c6
MatteoSpinelli <matteospinelli10@gmail.com>
2023-04-08 17:18:56 +0200

    add traslation

--- src/components/Nav/Menu.js
+++ src/components/Nav/Menu.js
@@ -4,12 +4,14 @@ import { ReactComponent as Cross } from "../../assets/svg/global/cross.svg"
 import { useRef } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { toggle } from "../../store/slices/menuSlice";
+import useTranslate from "../../hooks/useTranslate";
 
 
 export function Menu() {
     const { isDark } = useTheme()
     const menuRef = useRef()
     const dispatch = useDispatch()
+    const { nav_login, nav_about, nav_mission } = useTranslate("homepage")
     const isMenuVisible = useSelector((state) => state.isMenuVisible)
     return (
         <div ref={menuRef} className="menu_nav_global flex flex-col fixed top-0 w-8/12 h-screen justify-evenly items-center md:flex-row-reverse md:translate-x-[-2000px] md:relative md:w-3/5 md:h-auto md:bg-none md:justify-start md:gap-10 " style={{
@@ -19,10 +21,10 @@ export function Menu() {
 
         }}>
             <Button variant="login">
-                Login
+                {nav_login}
             </Button>
-            <div className="menu_nav_items cursor-pointer hover:text-link hover:underline hover:underline-offset-4 hover:decoration-2">About Us</div>
-            <div className="menu_nav_items cursor-pointer hover:text-link hover:underline hover:underline-offset-4 hover:decoration-2">Our Mission</div>
+            <div className="menu_nav_items cursor-pointer hover:text-link hover:underline hover:underline-offset-4 hover:decoration-2">{nav_about}</div>
+            <div className="menu_nav_items cursor-pointer hover:text-link hover:underline hover:underline-offset-4 hover:decoration-2">{nav_mission}</div>
             <div className="menu_nav_items cursor-pointer hover:text-link hover:underline hover:underline-offset-4 hover:decoration-2">Blog</div>
             <Cross onClick={() => {
                 dispatch(toggle())
