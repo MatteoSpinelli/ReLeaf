d7ab38b17d0ccc6c3e0d79141018e77bd349f0a3
AndreaPossidente <ndrpssdnt@gmail.com>
2023-04-12 17:49:24 +0200

    fix: unhandled promise rejection

--- server/routes/api/v1/calculator.js
+++ server/routes/api/v1/calculator.js
@@ -9,18 +9,25 @@ router.post("/", async function (req, res, next) {
   const body = req.body;
   const token = await prisma.authToken.findFirst();
 
-  const data = await fetch("https://api.footprintcalculator.org/v2/calculate", {
-    method: "POST",
-    headers: {
-      authorization: "Bearer " + token.token,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
+  try {
+    const data = await fetch(
+      "https://api.footprintcalculator.org/v2/calculate",
+      {
+        method: "POST",
+        headers: {
+          authorization: `Bearer ${token.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
 
-  const json = await data.json();
+    const json = await data.json();
 
-  res.status(200).json(json);
+    res.status(200).json(json);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 module.exports = router;
