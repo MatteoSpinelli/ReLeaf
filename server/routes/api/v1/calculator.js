a9b2648fa3512a8eb279f06690d229da2f74369c
AndreaPossidente <ndrpssdnt@gmail.com>
2023-04-12 18:47:46 +0200

    fix: unhandled promise rejection

--- server/routes/api/v1/calculator.js
+++ server/routes/api/v1/calculator.js
@@ -5,15 +5,17 @@ var router = express.Router();
 /* GET /api/v1/calculator */
 router.post("/", async function (req, res, next) {
   const body = req.body;
-  try {
-    const token = await prisma.authToken.findFirst({ where: {} });
+  const token = await prisma.authToken.findFirst();
 
+  console.log("Token Object is: ", token);
+
+  if (token) {
     const data = await fetch(
       "https://api.footprintcalculator.org/v2/calculate",
       {
         method: "POST",
         headers: {
-          authorization: `Bearer ${token.token}`,
+          authorization: `Bearer ${token?.token}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify(body),
@@ -23,8 +25,8 @@ router.post("/", async function (req, res, next) {
     const json = await data.json();
 
     res.status(200).json(json);
-  } catch (error) {
-    res.status(400).json({ error });
+  } else {
+    res.status(400).json({ error: "token is undefined" });
   }
 });
 
