68d85d7565fc43fe15ce5c52f0159eb9b9c78c16
MatteoSpinelli <matteospinelli10@gmail.com>
2023-04-22 02:55:29 +0200

    initial commit

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -5,8 +5,12 @@ import { ShowQuestion } from "./ShowQuestion"
 import { useEffect } from "react"
 import { redirect, useNavigate } from "react-router-dom"
 import { useUser } from "../../hooks/useUser"
+import { ReactComponent as NextButton } from "../../assets/svg/test/next.svg"
+import { ReactComponent as PreviousButton } from "../../assets/svg/test/previous.svg"
 import { ReactComponent as BG1 } from "../../assets/questionary-bg/quest-carpool.svg"
 import { ReactComponent as BG2 } from "../../assets/questionary-bg/quest-energy-eco.svg"
+
+import "./Test.scss"
 
 
 export function Test() {
@@ -16,7 +20,9 @@ export function Test() {
     const dispatch = useDispatch()
     const questions = useSelector((state: any) => state.questions)
     function handleButtons(evt: any){
-        dispatch(go(evt.target.innerText === "Next" ? 1 : -1))
+        const classValue = evt.target.classList.value.split(" ")[0]
+        evt.target.classList.add("button_active")
+        dispatch(go(classValue === "next_test" ? 1 : -1))
         /* upload of the answer */
         if (questions.currentQuestion === 16){
             navigate("/test-result")
@@ -28,12 +34,12 @@ export function Test() {
         }
     }, [])
     return (
-        <div className="w-screen h-screen flex flex-col justify-center items-center z-10">
+        <div className="testPage w-screen h-screen flex flex-col justify-center items-center z-10">
             {/* Presentational logic */}
             {data && <ShowQuestion question={data[questions.currentQuestion]} />} {/* Presentational logic for questions */}
             {/* next and prev buttons */}
-            <button onClick={handleButtons}>Previous</button>
-            <button onClick={handleButtons}>Next</button>
+            <PreviousButton className="previous_test bg-white fixed left-0 top-2/4 mx-4 rounded-full p-2" onClick={handleButtons} />
+            <NextButton className="next_test bg-white fixed right-0 top-2/4 mx-4 rounded-full p-2" onClick={handleButtons} />
             {/* Progession Circle and statements */}
             { questions.currentQuestion === 1 && <BG1 className="fixed bottom-0 left-0 z-[-2]" /> }
             { questions.currentQuestion === 2 && <BG2 className="fixed bottom-0 left-0 z-[-2]" /> }
