8dff64ebb4aee07e75cf5a0e81802019f6b47880
AndreaPossidente <ndrpssdnt@gmail.com>
2023-05-06 15:55:20 +0200

    fix: clear jwt cookie in auth middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,14 +1,15 @@
-const jwt = require("jsonwebtoken");
-const error = require("../utils/error");
+const jwt = require("jsonwebtoken")
+const error = require("../utils/error")
 
-const { ACCESS_TOKEN_SECRET } = process.env;
+const { ACCESS_TOKEN_SECRET } = process.env
 
 const authStrictMiddleware = async (req, res, next) => {
-  const { authorization } = req.headers;
+  const { authorization } = req.headers
 
   // if access token is missing deny the request
   if (!authorization) {
-    req.user = null;
+    req.user = null
+    res.clearCookie("jwt")
     res
       .status(401)
       .json(
@@ -16,16 +17,17 @@ const authStrictMiddleware = async (req, res, next) => {
           401,
           "The request has not been applied because it lacks valid authentication credentials for the target resource"
         )
-      );
-    return;
+      )
+    return
   }
 
-  const bearerToken = authorization.split(" ")[1];
+  const bearerToken = authorization.split(" ")[1]
 
   await jwt.verify(bearerToken, ACCESS_TOKEN_SECRET, (err, decoded) => {
     // if authentication is not valid deny the request
     if (!decoded) {
-      req.user = null;
+      req.user = null
+      res.clearCookie("jwt")
       res
         .status(401)
         .json(
@@ -33,38 +35,39 @@ const authStrictMiddleware = async (req, res, next) => {
             401,
             "The request has not been applied because it lacks valid authentication credentials for the target resource"
           )
-        );
-      return;
+        )
+      return
     }
     // if authentication is valid set the user in the request
-    req.user = decoded;
-    next();
-  });
-};
+    req.user = decoded
+    next()
+  })
+}
 
 const authMiddleware = async (req, res, next) => {
-  const { authorization } = req.headers;
+  const { authorization } = req.headers
 
   // if access token exists
   if (authorization) {
     // get the access token from bearer header
-    const accessToken = authorization.split(" ")[1];
+    const accessToken = authorization.split(" ")[1]
 
     // verify that the access token is valid and not exipred
     await jwt.verify(accessToken, ACCESS_TOKEN_SECRET, (err, decoded) => {
       // if the access token is not valid or expired unset the user and proceed to the next middleware
       if (!decoded) {
-        req.user = null;
-        next();
-        return;
+        req.user = null
+        res.clearCookie("jwt")
+        next()
+        return
       }
 
       // if the access token is valid send it through the request to the next middleware
-      req.user = { ...decoded, accessToken: authorization };
-    });
+      req.user = { ...decoded, accessToken: authorization }
+    })
   }
   // go to the next middleware
-  next();
-};
+  next()
+}
 
-module.exports = { authStrictMiddleware, authMiddleware };
+module.exports = { authStrictMiddleware, authMiddleware }
