b57bd4ccbe360459badf0564e6696c5083f5f248
AndreaPossidente <ndrpssdnt@gmail.com>
2023-05-06 15:39:37 +0200

    fix: set jwt cookie directly from server

--- server/app.js
+++ server/app.js
@@ -1,58 +1,69 @@
-require("dotenv").config();
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
+require("dotenv").config()
+const createError = require("http-errors")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
+const cors = require("cors")
 
-const rootRoutes = require("./routes/api/v1");
-const authRoutes = require("./routes/api/v1/auth.js");
-const questionsRoutes = require("./routes/api/v1/questions.js");
-const activitiesRoutes = require("./routes/api/v1/activities.js");
-const tokenRoutes = require("./routes/api/v1/token.js");
-const calculatorRoutes = require("./routes/api/v1/calculator.js");
-const { authMiddleware } = require("./middlewares/auth");
-const app = express();
+const rootRoutes = require("./routes/api/v1")
+const authRoutes = require("./routes/api/v1/auth.js")
+const questionsRoutes = require("./routes/api/v1/questions.js")
+const activitiesRoutes = require("./routes/api/v1/activities.js")
+const tokenRoutes = require("./routes/api/v1/token.js")
+const calculatorRoutes = require("./routes/api/v1/calculator.js")
+const { authMiddleware } = require("./middlewares/auth")
+const app = express()
 
-app.use(cors());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(authMiddleware);
+var whitelist = ["http://localhost:3000", "https://re-leaf.vercel.app/"]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+}
+
+app.use(cors({ ...corsOptions, credentials: true }))
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+app.use(authMiddleware)
 
 // api routes
-app.use("/api/v1", rootRoutes);
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/questions", questionsRoutes);
-app.use("/api/v1/activities", activitiesRoutes);
-app.use("/api/v1/token", tokenRoutes);
-app.use("/api/v1/calculator", calculatorRoutes);
+app.use("/api/v1", rootRoutes)
+app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/questions", questionsRoutes)
+app.use("/api/v1/activities", activitiesRoutes)
+app.use("/api/v1/token", tokenRoutes)
+app.use("/api/v1/calculator", calculatorRoutes)
 
 app.get("/", function (req, res, next) {
-  res.status(200).json({ message: "index" });
-});
+  res.status(200).json({ message: "index" })
+})
 
 app.get("/api", function (req, res, next) {
-  res.status(200).json({ message: "index" });
-});
+  res.status(200).json({ message: "index" })
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404, "endpoint not found"));
-});
+  next(createError(404, "endpoint not found"))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.json({ status: err.status || 500, message: err.message });
-});
+  res.status(err.status || 500)
+  res.json({ status: err.status || 500, message: err.message })
+})
 
-module.exports = app;
+module.exports = app
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState } from "react"
 
 interface AuthResponse {
-  success: boolean;
-  message: string;
-  accessToken: string;
-  iat: number;
-  exp: number;
+  success: boolean
+  message: string
+  accessToken: string
+  iat: number
+  exp: number
 }
 
 export default function useAuth() {
-  const [authenticated, setAuthenticated] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false)
 
   const login = async (email: string, password: string) => {
     const res = await fetch(
@@ -19,21 +19,21 @@ export default function useAuth() {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify({ email, password }),
       }
-    );
-    const data: AuthResponse = await res.json();
-    const dat = new Date(data.exp * 1000).toUTCString();
-    console.log(dat);
+    )
+    const data: AuthResponse = await res.json()
+    const dat = new Date(data.exp * 1000).toUTCString()
+    console.log(dat)
     if (data && data.success) {
-      setAuthenticated(true);
-      document.cookie = `jwt=${data.accessToken};expires=${dat};secure;sameSite=Strict;path=/;`;
-      return { success: true };
+      setAuthenticated(true)
+      return { success: true }
     } else {
-      setAuthenticated(false);
-      return { success: false, data };
+      setAuthenticated(false)
+      return { success: false, data }
     }
-  };
+  }
 
-  return { login, authenticated };
+  return { login, authenticated }
 }
